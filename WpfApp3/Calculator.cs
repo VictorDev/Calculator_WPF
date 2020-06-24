17ecd136aabe6e91c389b7a30958c4a48dacb2ea
VictorDev <nirvana01_01@mail.ru>
2020-06-24 18:24:48 +0300

    Initial commit

---
+++ WpfApp3/Calculator.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp3
+{
+    enum Operation { Add, Div, Sub, Mul, Sqrt, Pow, Tan, Log, Sin, Cos, Interest }
+
+    delegate void CalculatorDidUpdateOutput(Calculator sender, double value, int precision);
+
+    class Calculator
+    {
+        double? left = null;
+        double? right = null;
+        Operation? currentOp = null;
+        bool decimalPoint = false;
+        int precision = 0;
+
+        public event CalculatorDidUpdateOutput DidUpdateValue;
+        public event EventHandler<string> InputError;
+        public event EventHandler<string> CalculationError;
+
+        public void AddDigit(int digit)
+        {
+            if (left.HasValue && Math.Log10(left.Value) > 10 || precision > 13)
+            {
+                InputError?.Invoke(this, "Input overflow");
+                return;
+            }
+            if (precision > 10)
+            {
+                InputError?.Invoke(this, "Input overflow");
+                return;
+            }
+            if (!decimalPoint)
+            {
+                if (left >= 0 || left==null)
+                {
+                    left = (left ?? 0) * 10 + digit;
+                }
+                else {
+                    left = (left ?? 0) * 10 - digit;
+                }
+            }
+            else
+            {
+                precision += 1;
+                left = left + (Math.Pow(0.1, precision) * digit);
+            }
+            DidUpdateValue?.Invoke(this, left.Value, precision);
+        }
+
+        public void AddDecimalPoint()
+        {
+            if (!left.HasValue)
+            {
+                left = 0;
+            }
+            decimalPoint = true;
+        }
+
+        public void AddOperation(Operation op)
+        {
+            if (left.HasValue && currentOp.HasValue)
+            {
+                Compute();
+
+            }
+            if (!right.HasValue && !(op == Operation.Sqrt || op == Operation.Cos || op == Operation.Log || op == Operation.Tan || op == Operation.Sin))
+            {
+                right = left;
+                left = 0;
+                precision = 0;
+                decimalPoint = false;
+                DidUpdateValue.Invoke(this, left.Value, precision);
+                currentOp = op;
+            }
+            if (left.HasValue && op == Operation.Sqrt || op == Operation.Cos || op == Operation.Log || op == Operation.Tan || op == Operation.Sin || op == Operation.Interest)
+            {
+                currentOp = op;
+                ComputeUnar();
+                currentOp = null;
+            }
+            currentOp = op;
+
+        }
+
+        public void ComputeUnar()
+        {
+            switch (currentOp)
+            {
+                
+                case Operation.Sqrt:
+                    if (left < 0)
+                    {
+                        InputError?.Invoke(this, "Error");
+                        return;
+                    }
+                    if (left != null)
+                        right = Math.Sqrt((double)left);
+                    else right = Math.Sqrt((double)right);
+                    break;
+
+                case Operation.Log:
+                    if (left < 0)
+                    {
+                        InputError?.Invoke(this, "Error");
+                        return;
+                    }
+                    if (left != null)
+                        right = Math.Log((double)left);
+                    else right = Math.Log((double)right);
+
+                    break;
+
+                case Operation.Sin:
+                    if (left != null)
+                        right = Math.Sin((double)left);
+                    else right = Math.Cos((double)right);
+                    
+                    break;
+
+                case Operation.Cos:
+                    if(left!=null)
+                        right = Math.Cos((double)left);
+                    else right = Math.Cos((double)right);
+                    break;
+
+                case Operation.Tan:
+                    if (left != null)
+                        right = Math.Tan((double)left);
+                    else right = Math.Tan((double)right);
+                    break;
+
+                case Operation.Interest:
+                    right = (double)right / 100;
+                    break;
+            }
+            left = right;
+            DidUpdateValue?.Invoke(this, right.Value, precision);
+            right = null;
+        }
+        public void Sign()
+        {
+            if (left != null)
+            {
+                left = left * (-1);
+                DidUpdateValue?.Invoke(this, left.Value, precision);
+            }
+            else {
+                right = right * (-1);
+                DidUpdateValue?.Invoke(this, right.Value, precision);
+            }
+        }
+
+        public void Compute()
+        {
+            switch (currentOp)
+            {
+                case Operation.Add:
+                    right = left + right;
+                    left = null;
+                    break;
+
+                case Operation.Sub:
+                    right = right - left;
+                    left = null;
+                    break;
+
+                case Operation.Mul:
+                    right = left * right;
+                    left = null;
+                    break;
+
+                case Operation.Div:
+                    if (left == 0)
+                    {
+                        CalculationError?.Invoke(this, "Division by 0!");
+                        return;
+                    }
+                    right = right / left;
+                    left = null;
+                    break;
+
+                case Operation.Pow:
+                    right = Math.Pow((double)right, (double)left);
+                    left = null;
+                    break;
+                default:
+                    currentOp = null;
+                    precision = 0;
+                    decimalPoint = false;
+                    return;
+            }
+            left = right;
+            DidUpdateValue?.Invoke(this, right.Value, precision);
+            right = null;
+            currentOp = null;
+        }
+
+        public void Clear()
+        {
+            left = 0;
+            DidUpdateValue?.Invoke(this, left.Value, precision);
+        }
+        public void Reset()
+        {
+            currentOp = null;
+            left = 0;
+            right = null;
+            precision = 0;
+            decimalPoint = false;
+            DidUpdateValue?.Invoke(this, left.Value, 0);
+        }
+        public void ClearSimbol()
+        {
+            if (decimalPoint)
+            {
+                left = left - (left * Math.Pow(10, precision - 1) % 1) * Math.Pow(0.1, precision - 1);
+                precision--;
+                if (precision == 0)
+                    decimalPoint = false;
+            }
+            else
+            {
+                left = (int)(left * 0.1);
+            }
+            DidUpdateValue?.Invoke(this, left.Value, precision);
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        Thickness onMouseDown;
+        Thickness onMouseUp;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+            calc.DidUpdateValue += calc_DidUpdate;
+            calc.InputError += calc_Error;
+            calc.CalculationError += calc_Error;
+            onMouseDown = new Thickness(3);
+            onMouseUp = new Thickness(1);
+        }
+
+        private void calc_Error(object sender, string e)
+        {
+            MessageBox.Show(e, "Calculator Error",MessageBoxButton.OK,MessageBoxImage.Warning);
+        }
+
+        private void calc_DidUpdate(Calculator sender, double value, int precision)
+        {
+            Result_Text_Block.Text = $"{value}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = (sender as Button).Content.ToString();
+            string name = (sender as Button).Name;
+            int digit;
+            if (int.TryParse(text, out digit))
+            {
+                calc.AddDigit(digit);
+            }
+            else
+            {
+                switch (name)
+                {
+                    case "dec":
+                        calc.AddDecimalPoint();
+                        break;
+                    case "evaluate":
+                        calc.Compute();
+                        break;
+                    case "addition":
+                        calc.AddOperation(Operation.Add);
+                        break;
+                    case "substraction":
+                        calc.AddOperation(Operation.Sub);
+                        break;
+                    case "multiplication":
+                        calc.AddOperation(Operation.Mul);
+                        break;
+                    case "division":
+                        calc.AddOperation(Operation.Div);
+                        break;
+                    case "clear":
+                        calc.Clear();
+                        break;
+                    case "reset":
+                        calc.Reset();
+                        break;
+                    case "back":
+                        calc.ClearSimbol();
+                        break;
+                    case "pow":
+                        calc.AddOperation(Operation.Pow);
+                        break;
+                    case "sqrt":
+                        calc.AddOperation(Operation.Sqrt);
+                        break;
+                    case "log":
+                        calc.AddOperation(Operation.Log);
+                        break;
+                    case "tan":
+                        calc.AddOperation(Operation.Tan);
+                        break;
+                    case "Cos":
+                        calc.AddOperation(Operation.Cos);
+                        break;
+                    case "Sin":
+                        calc.AddOperation(Operation.Sin);
+                        break;
+                    case "inter":
+                        calc.AddOperation(Operation.Interest);
+                        break;
+                    case "sign":
+                        calc.Sign();
+                        break;
+                    case "exit":
+                        this.Close();
+                        break;
+                }
+            }
+        }
+
+        private void Mouse_Down(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Button).BorderThickness = onMouseDown;
+        }
+
+        private void Mouse_Up(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Button).BorderThickness = onMouseUp;
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D1:
+                case Key.NumPad1:
+                    calc.AddDigit(1);
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    calc.AddDigit(2);
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    calc.AddDigit(3);
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    calc.AddDigit(4);
+                    break;
+                case Key.D5:
+                case Key.NumPad5:
+                    calc.AddDigit(5);
+                    break;
+                case Key.D6:
+                case Key.NumPad6:
+                    calc.AddDigit(6);
+                    break;
+                case Key.D7:
+                case Key.NumPad7:
+                    calc.AddDigit(7);
+                    break;
+                case Key.D8:
+                case Key.NumPad8:
+                    calc.AddDigit(8);
+                    break;
+                case Key.D9:
+                case Key.NumPad9:
+                    calc.AddDigit(9);
+                    break;
+                case Key.D0:
+                case Key.NumPad0:
+                    calc.AddDigit(0);
+                    break;
+                case Key.Add:
+                    calc.AddOperation(Operation.Add);
+                    break;
+                case Key.Back:
+                    calc.ClearSimbol();
+                    break;
+                case Key.Decimal:
+                    calc.AddDecimalPoint();
+                    break;
+                case Key.Delete:
+                    calc.Clear();
+                    break;
+                case Key.Divide:
+                    calc.AddOperation(Operation.Div);
+                    break;
+                case Key.Multiply:
+                    calc.AddOperation(Operation.Mul);
+                    break;
+                case Key.Subtract:
+                    calc.AddOperation(Operation.Sub);
+                    break;
+                case Key.Enter:
+                    calc.Compute();
+                    break;
+
+            }
+        }
+    }
+}
